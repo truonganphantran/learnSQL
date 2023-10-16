a3dc441c305918e41028263b0fd8982c113b88fa
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-16 17:14:37 +0700

    Update Julie.sql

--- Julie.sql
+++ Julie.sql
@@ -5,3 +5,9 @@ COUNT(CASE WHEN skill IN ( 'Python', 'Tableau', 'PostgreSQL') then skill
 END ) 
 FROM candidates
 GROUP BY candidate_id
+
+--cu phap vieet hoa ten chu cai đầu
+SELECT user_id 
+  , CONCAT( UPPER (LEFT( LOWER (name),1)) , RIGHT( LOWER (name),LENGTH (name)-1)) AS name
+FROM Users
+ORDER BY user_id;
