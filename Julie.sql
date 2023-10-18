b60d424b125a7823b7a81c2ec0631abb10f7f900
truonganphantran <147529962+truonganphantran@users.noreply.github.com>
2023-10-18 20:01:47 +0700

    Update Julie.sql

--- Julie.sql
+++ Julie.sql
@@ -11,3 +11,16 @@ SELECT user_id
   , CONCAT( UPPER (LEFT( LOWER (name),1)) , RIGHT( LOWER (name),LENGTH (name)-1)) AS name
 FROM Users
 ORDER BY user_id;
+
+
+---su khac nhau cua 2 cu phap sau (https://datalemur.com/questions/laptop-mobile-viewership)
+SELECT  
+SUM(CASE WHEN  device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+SUM(CASE WHEN device_type IN ( 'tablet', 'phone') THEN 1 ELSE 0 END) AS mobile_views
+FROM viewership
+
+SELECT  
+SUM(CASE WHEN  device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+SUM(CASE WHEN device_type IN ( 'tablet', 'phone') THEN 1 ELSE 0 END) AS mobile_views
+FROM viewership
+GROUP BY device_type
